974be8ce252729408c6ec554765b6d594bc53d34
Gary Siu <Gary.Siu@matchesfashion.com>
2022-11-24 04:15:51 +0000

    initial commit

---
+++ api/handler.mjs
@@ -0,0 +1,32 @@
+import fetch from 'node-fetch'
+
+export default function handler(request, response) {
+  const hashPattern = /a\/(\w+)$/
+  if (!hashPattern.test(request.query['album'])) {
+    return response.status(400).send('bad request');
+  }
+
+  const albumHash = request.query['album'].match(hashPattern)[1]
+  const myHeaders = new Headers();
+  myHeaders.append("Authorization", `Client-ID ${process.env.CLIENT_ID}`);
+
+  var requestOptions = {
+    method: 'GET',
+    headers: myHeaders,
+    redirect: 'follow'
+  };
+
+  fetch(`https://api.imgur.com/3/album/${albumHash}`, requestOptions)
+  .then(res => res.text())
+  .then(result => {
+    const output = JSON.stringify(JSON.parse(result).data.images.map(image => image.link))
+    .replace(/[\[|\]]/g, '')
+    .replace(/,/g, ',\n');
+    response.setHeader('Content-Type', 'text/plain')
+    response.status(200).send(output);
+  })
+  .catch(error => {
+    console.log('error', error)
+    return response.status(400).send('bad request');
+  });
+}
