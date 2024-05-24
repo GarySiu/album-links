a23080d599396e1fca2a70d96c5e851f211d51aa
Gary Siu <Gary@untypeable.com>
2024-05-24 16:19:08 +0100

    update regex

--- api/handler.mjs
+++ api/handler.mjs
@@ -1,5 +1,5 @@
 export default function handler(request, response) {
-  const hashPattern = /(?:a|gallery)\/(\w+|[a-z0-9\-]+)(?:#|$)/i
+  const hashPattern = /(?:a|gallery)\/(?:(?<!\/)[a-z0-9\-]+)|(\w+)(?:#|$)/i
   if (!hashPattern.test(request.query['album'])) {
     return response.status(400).send('bad request');
   }
